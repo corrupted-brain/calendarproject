510c65ab7beb776aab9af0536bc08f2ef5fc55d5
kemish bajgai <kemishbajgai4@gmail.com>
2023-02-01 10:30:44 +0545

    yes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-
-let btn1 = document.querySelector("#appointment-type");
-btn1.addEventListener("change", function (e) {
+let btn = document.querySelector("#appointment-type");
+btn.addEventListener("change", function (e) {
     e.preventDefault();
     if (this.value === "ad") {
-document.querySelector("#calendar").style.display = "block";
-    } else {
+        document.querySelector("#calendar").style.display = "block";
+        document.querySelector("#nepali-datepicker").style.display = "none";
+    } else if (this.value === "bs") {
+        document.querySelector("#nepali-datepicker").style.display = "block";
         document.querySelector("#calendar").style.display = "none";
     }
 })
-
-           
+let date = document.querySelector("#calendar")
+console.log(date)
