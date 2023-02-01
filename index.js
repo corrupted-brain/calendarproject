3648edffce236e1ba4ed50154abadf34ffab7538
kailash0x01 <kailashbohara0x00@gmail.com>
2023-02-01 18:00:56 +0545

    Update index.js
    Fixed

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ let btn = document.querySelector("#appointment-type");
 btn.addEventListener("change", function (e) {
     e.preventDefault();
     if (this.value === "ad") {
-        document.querySelector("#calendar").style.display = "block";
+        document.querySelector("#english-datepicker").style.display = "block";
         document.querySelector("#nepali-datepicker").style.display = "none";
     } else if (this.value === "bs") {
         document.querySelector("#nepali-datepicker").style.display = "block";
-        document.querySelector("#calendar").style.display = "none";
+        document.querySelector("#english-datepicker").style.display = "none";
     }
-})
-let date = document.querySelector("#calendar")
-console.log(date)
+});
+
+window.onload = function() {
+    var mainInput = document.getElementById("nepali-datepicker");
+    mainInput.nepaliDatePicker();
+};
